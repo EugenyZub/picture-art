406ba44c9380f85adcf0dfc20f5fcc15c53fc491
Ëugene <evgeniys1@tut.by>
2018-11-01 22:10:28 +0300

    кнопка с добавлением стилей

--- src/js/parts/styles.js
+++ src/js/parts/styles.js
@@ -1,7 +1,24 @@
 function moreStyles() {
-    let buttonStyles = document.querySelectorAll('.button-styles');
+    let buttonStyles = document.querySelector('.button-styles'),
+        newStyles = document.querySelectorAll('.styles-2');
+    
+    buttonStyles.addEventListener('click', ()=> {
+        newStyles.forEach( function(elem) {
+            elem.style.display = 'block';
 
+            elem.classList.remove('hidden-lg');
+            elem.classList.remove('hidden-md');
+            elem.classList.remove('hidden-sm');
+            elem.classList.remove('hidden-xs');
 
+            elem.classList.add('col-sm-3');
+            elem.classList.add('col-sm-offset-0');
+            elem.classList.add('col-xs-10');
+            elem.classList.add('col-xs-offset-1');
+        });
+
+        buttonStyles.style.display = 'none';
+    });
 }
 
 export default moreStyles;
