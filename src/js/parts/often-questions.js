3b513d683af44a60f8c61227384d493841335508
Eugene <evgeniys1@tut.by>
2018-11-02 13:40:05 +0300

    often-questions update

--- src/js/parts/often-questions.js
+++ src/js/parts/often-questions.js
@@ -1,30 +1,32 @@
 function oftenQuestions() {
     let accordionBlock = document.querySelectorAll('.accordion-block'),
-        accordionHeading = document.querySelectorAll('.accordion-heading'),
-        
+        accordionHeading = document.querySelectorAll('.accordion-heading'),   
         accordion = document.querySelectorAll('#accordion div');
+
     //hidden-lg hidden-md hidden-sm hidden-xs
     accordionBlock.forEach((elem)=> {
         elem.classList.add('hidden-lg');   
     });
     
-    accordionHeading.forEach((elem, index)=> {
-        elem.addEventListener('click', ()=> {
+    accordionHeading.forEach((elem2, index)=> {
+       let accordionHeadingSpan = elem2.querySelector('span');     
+
+        elem2.addEventListener('click', ()=> {
             accordion[index].classList.toggle('hidden-lg');
             accordion[index].style.marginBottom = '20px';
             accordion[index].style.marginTop = '20px';
-
-            let accordionHeadingSpan = elem.querySelector('span');
-            console.log(accordionHeadingSpan);
+            let curentIndex = index;
             accordionHeadingSpan.classList.toggle('active');
-        });
-        
-        elem.forEach( ()=> {
-
-        });
-    });
-
-    
+           
+            accordionBlock.forEach((elem3, index2)=> {
+                if(index2 != curentIndex) {
+                    elem3.classList.add('hidden-lg');
+                    accordionHeadingSpan = accordionHeading[index2].querySelector('span');
+                    accordionHeadingSpan.classList.remove('active');    
+                }  
+            });
+        });   
+    });  
 }
 
 export default oftenQuestions;
